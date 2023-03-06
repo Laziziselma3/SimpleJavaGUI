af014a239d8f0ff64ef37bd478cb3be2d3a51c53
Selma Lazizi <selma.lazizi@ensia.edu.dz>
2023-03-06 16:03:57 +0100

    Hello world

---
+++ src/Hello_world/Start.java
@@ -0,0 +1,11 @@
+package Hello_world;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.print("Hello world ");
+        
+	}
+
+}
